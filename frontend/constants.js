e4fda1c2fc716f5aaf7233363d905f3cc8c97dc4
Yu Min Tan <s10195143@connect.np.edu.sg>
2022-02-06 22:44:09 +0800

    Correct backendurl

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,2 +1,2 @@
-export const backendUrl = "http://localhost:8141/classOffers";
+export const backendUrl = "http://backend:8141/classOffers";
 export const sessionUrl = "http://10.31.11.11:8090/session";
